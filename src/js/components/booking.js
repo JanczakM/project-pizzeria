0aa93eff9fbe02d19f87b6e92f11ff0c575c6d45
Małgorzata Jańczak <m.janczak@int.pl>
2019-10-10 20:11:29 +0200

    add hourpicker

--- src/js/components/booking.js
+++ src/js/components/booking.js
@@ -1,6 +1,7 @@
 import {templates, select} from '../settings.js';
 import {AmountWidget} from './amountwidget.js';
 import {DatePicker} from './datepicker.js';
+import {HourPicker} from './hourpicker.js';
 
 export class Booking{
   constructor(bookingContainer){
@@ -19,6 +20,7 @@ export class Booking{
     thisBooking.dom.peopleAmount = thisBooking.dom.wrapper.querySelector(select.booking.peopleAmount);
     thisBooking.dom.hoursAmount = thisBooking.dom.wrapper.querySelector(select.booking.hoursAmount);
     thisBooking.dom.datePicker = thisBooking.dom.wrapper.querySelector(select.widgets.datePicker.wrapper);
+    thisBooking.dom.hourPicker = thisBooking.dom.wrapper.querySelector(select.widgets.hourPicker.wrapper);
   }
 
   initWidgets(){
@@ -27,5 +29,6 @@ export class Booking{
     thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
     thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
     thisBooking.datePicker = new DatePicker(thisBooking.dom.datePicker);
+    thisBooking.hourPicker = new HourPicker(thisBooking.dom.hourPicker);
   }
 }
